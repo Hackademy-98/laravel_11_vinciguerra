a2c1a76b5dd1d811abd1851795bf6064b8450a9b
Michele Vinciguerra <michele-09@hotmail.it>
2023-11-20 18:40:04 +0100

    laravel 11 vinciguerra

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 p-5 text-white">
+                    <div class="backdrop-blur p-5 rounded-4">
+                        <h1>epay</h1>
+                        <p class="fs-3">compra o sarai bannato</p>
+                        <button class="btn btn-primary">bannati</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+</x-layout>
